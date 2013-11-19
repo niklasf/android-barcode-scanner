2843d45f4eed4eee3c42a02bb9fcbd10ef35797b
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-11-19 11:15:08 +0100

    Draw a random QR code

--- web.js
+++ web.js
@@ -1,11 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    qrcode = require('qrcode'),
+    crypto = require('crypto');
 
 var app = express();
 
 app.use(express.logger());
 
 app.get('/', function (req, res) {
-    res.end("Hello world!");
+    res.setHeader('Content-Type', 'text/plain');
+
+    crypto.randomBytes(20, function (err, buffer) {
+        if (err) {
+            throw err;
+        }
+
+        var token = buffer.toString('hex');
+        console.log(token);
+
+        qrcode.draw(token, function (err, canvas) {
+            if (err) {
+                throw err;
+            }
+
+            canvas.pngStream().pipe(res);
+        });
+    });
 });
 
 app.listen(process.env.PORT || 80, function () {
