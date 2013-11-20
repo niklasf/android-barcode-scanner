a06c111fe7a650ae28dfdedf53273bd23b298fb1
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2013-11-21 00:28:07 +0100

    Factor out build of static-build.bat

--- client/fix-static-release-manifest.bat
+++ client/fix-static-release-manifest.bat
@@ -1,22 +1,24 @@
 rem Build a static version of Qt
 rem ============================
 rem
-rem 1. Download the Qt library source code to C:\Qt4.8.5.
+rem 1. Download the Qt library source code to C:\Qt\4.8.5
 rem    Source: http://qt-project.org/downloads#qt-lib
 rem
-rem 2. Download OpenSSL to C:\OpenSSL.
+rem 2. Download OpenSSL to C:\OpenSSL
 rem    Source: http://slproweb.com/products/Win32OpenSSL.html
 rem
-rem 3. Build in C:\Qt4.8.5:
+rem 3. Build in C:\Qt\4.8.5
 rem
-rem    configure -openssl -static -release -platform win32-msvc2012
+rem    configure -openssl -I C:\OpenSSL\include -L C:\OpenSSL -static -release -platform win32-msvc2012
 rem    nmake sub-src
 
 rem Build a static application
 rem ==========================
+rem qmake.exe -spec win32-msvc2012 android-barcode-scanner-client.pro 
+rem nmake release
 
-C:\Qt4.8.5\bin\qmake.exe -spec win32-msvc2012 android-barcode-scanner-client.pro 
-nmake release
+rem Fix the manifest
+rem ================
 cd release
 mt.exe -manifest android-barcode-scanner-client.exe.manifest -outputresource:android-barcode-scanner-client.exe;1
 cd ..
